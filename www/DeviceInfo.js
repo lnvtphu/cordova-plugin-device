d11415f67c244fa9339717d5f5dba7604c38866d
Phu Le <lnvtphu@gmail.com>
2018-09-05 15:22:21 +0700

    update

--- www/DeviceInfo.js
+++ www/DeviceInfo.js
@@ -1,21 +1,5 @@
-var exec = cordova.require('cordova/exec');
+var exec = require('cordova/exec');
 
-var DeviceInfoA = function() {
-    console.log('InfoDevice instanced');
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'DeviceInfo', 'coolMethod', [arg0]);
 };
-
-DeviceInfoA.prototype.coolMethod = function(arg0, onSuccess, onError) {
-    var errorCallback = function(obj) {
-        onError(obj);
-    };
-
-    var successCallback = function(obj) {
-        onSuccess(obj);
-    };
-
-    exec(successCallback, errorCallback, 'DeviceInfoA', 'getInfo', [arg0]);
-};
-
-if (typeof module != 'undefined' && module.exports) {
-    module.exports = DeviceInfoA;
-}
